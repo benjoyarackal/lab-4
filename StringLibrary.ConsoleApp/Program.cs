5bffd92c968ea207dd7029b3f2c05966abaa8bce
Ben Joy <benjoyarakal@gmail.com>
2025-04-01 19:27:12 -0400

    Initial commit for Lab 4 - StringLibrary

---
+++ StringLibrary.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using StringLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+        Console.WriteLine($"Starts with uppercase? {input.StartsWithUpper()}");
+    }
+}
---
+++ StringLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace StringLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return false;
+            return char.IsUpper(str[0]);
+        }
+    }
+}
